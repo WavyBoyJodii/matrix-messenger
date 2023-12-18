7d9f5689ab905eb229b2d1c41fe56d0129d08a19
WavyBoyJodii <najeecasey@gmail.com>
2023-12-17 22:37:35 -0500

    create middleware to protect routes with getAuth

---
+++ src/middleware.ts
@@ -0,0 +1,17 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import getAuth from "./lib/getAuth";
+
+const protectedRoutes = ["/home"];
+
+export default async function middleware(req: NextRequest) {
+  if (protectedRoutes.includes(req.nextUrl.pathname)) {
+    const valid = await getAuth();
+    console.log(valid);
+    if (valid === false) {
+      return NextResponse.redirect(new URL("/", req.url));
+    } else {
+      return NextResponse.next();
+    }
+  }
+}
