0127a291ec075b943f782f824fae653334c0d5aa
WavyBoyJodii <najeecasey@gmail.com>
2023-12-22 05:18:14 -0500

    create getChat function to provide chat to chat page

---
+++ src/lib/getChat.ts
@@ -0,0 +1,22 @@
+"use server";
+
+import { cookies } from "next/headers";
+import type { Chat } from "./types";
+
+export default async function getChat({ chatId }: { chatId: string }) {
+  const cookieStore = cookies();
+  const token = cookieStore.get("auth");
+  console.log(token);
+  const result = await fetch(
+    `https://messengerbackend-production-d50f.up.railway.app/users/chat/${chatId}`,
+    {
+      headers: {
+        Authorization: `Bearer ${token?.value}`,
+      },
+      cache: "no-store",
+    },
+  );
+  const chat = (await result.json()) as Chat;
+
+  return chat;
+}
