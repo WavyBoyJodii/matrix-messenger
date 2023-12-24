69cad0122f1098ad47858654d6ec944f860cc42c
WavyBoyJodii <najeecasey@gmail.com>
2023-12-24 08:31:29 -0500

    add matcher to middleware for better route protection

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,16 +2,24 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import getAuth from "./lib/getAuth";
 
-const protectedRoutes = ["/add-friend", "/chat", "/friend-list", "/requests"];
+export const config = {
+  matcher: ["/add-friend", "/chat/:path*", "/friend-list", "/requests"],
+};
+
+// const protectedRoutes = [
+//   "/add-friend",
+//   "/chat/*/*",
+//   "",
+//   "",
+// ];
+// const entrancePages = ["/login", "/register"];
 
 export default async function middleware(req: NextRequest) {
-  if (protectedRoutes.includes(req.nextUrl.pathname)) {
-    const valid = await getAuth();
-    console.log(valid);
-    if (valid === false) {
-      return NextResponse.redirect(new URL("/", req.url));
-    } else {
-      return NextResponse.next();
-    }
+  const valid = await getAuth();
+  console.log(`logging middleware auth response ${valid}`);
+  if (valid === false) {
+    return NextResponse.redirect(new URL("/", req.url));
+  } else {
+    return NextResponse.next();
   }
 }
