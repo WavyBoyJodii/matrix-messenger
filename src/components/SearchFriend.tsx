b9a46a4526ecbb0ee72c15dbdd59e37e81f54bcf
WavyBoyJodii <najeecasey@gmail.com>
2023-12-26 04:46:13 -0500

    update searchfriend augment onsubmit to Get method api call

--- src/components/SearchFriend.tsx
+++ src/components/SearchFriend.tsx
@@ -19,12 +19,13 @@ import {
 } from "@/lib/types";
 import { SetStateAction, Dispatch } from "react";
 import axios from "axios";
+import getAuthToken from "@/lib/getAuthToken";
 
 interface SearchFriendProps {
   setFriend: Dispatch<SetStateAction<User | null>>;
 }
 
-export default function SearchFriend({ setFriend }: SearchFriendProps) {
+export default async function SearchFriend({ setFriend }: SearchFriendProps) {
   const form = useForm<ZRequestedUserSchema>({
     resolver: zodResolver(requestedUserSchema),
     defaultValues: {
@@ -33,10 +34,15 @@ export default function SearchFriend({ setFriend }: SearchFriendProps) {
   });
 
   const onSubmit = async (data: ZRequestedUserSchema) => {
+    const token = await getAuthToken();
     try {
-      const result = await axios.post<User>(
-        "https://messengerbackend-production-d50f.up.railway.app/users/find",
-        data,
+      const result = await axios.get<User>(
+        `https://messengerbackend-production-d50f.up.railway.app/users/findbyusername/${data.requestedUser}`,
+        {
+          headers: {
+            Authorization: `Bearer ${token?.value}`,
+          },
+        },
       );
       console.log(result);
       setFriend(result.data);
@@ -55,20 +61,17 @@ export default function SearchFriend({ setFriend }: SearchFriendProps) {
   };
 
   return (
-    <div className="flex w-1/2">
-      <Form {...form}>
-        <form
-          onSubmit={form.handleSubmit(onSubmit)}
-          className="space-y-2 sm:space-y-8 w-full flex place-content-center"
-        >
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="space-y-2 sm:space-y-8 flex justify-center gap-2 items-center place-content-center"
+      >
+        <div className=" flex justify-center gap-2">
           <FormField
             control={form.control}
             name="requestedUser"
             render={({ field }) => (
               <FormItem className="">
-                <FormLabel className=" flex place-content-center">
-                  Friend Username
-                </FormLabel>
                 <FormControl>
                   <Input autoComplete="off" {...field} />
                 </FormControl>
@@ -76,26 +79,25 @@ export default function SearchFriend({ setFriend }: SearchFriendProps) {
               </FormItem>
             )}
           />
-          <div className="flex gap-16 justify-between place-self-center">
-            <Button type="submit" className="flex place-self-center">
-              <svg
-                width="15"
-                height="15"
-                viewBox="0 0 15 15"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M10 6.5C10 8.433 8.433 10 6.5 10C4.567 10 3 8.433 3 6.5C3 4.567 4.567 3 6.5 3C8.433 3 10 4.567 10 6.5ZM9.30884 10.0159C8.53901 10.6318 7.56251 11 6.5 11C4.01472 11 2 8.98528 2 6.5C2 4.01472 4.01472 2 6.5 2C8.98528 2 11 4.01472 11 6.5C11 7.56251 10.6318 8.53901 10.0159 9.30884L12.8536 12.1464C13.0488 12.3417 13.0488 12.6583 12.8536 12.8536C12.6583 13.0488 12.3417 13.0488 12.1464 12.8536L9.30884 10.0159Z"
-                  fill="currentColor"
-                  fill-rule="evenodd"
-                  clip-rule="evenodd"
-                ></path>
-              </svg>
-            </Button>
-          </div>
-        </form>
-      </Form>
-    </div>
+
+          <Button type="submit">
+            <svg
+              width="15"
+              height="15"
+              viewBox="0 0 15 15"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M10 6.5C10 8.433 8.433 10 6.5 10C4.567 10 3 8.433 3 6.5C3 4.567 4.567 3 6.5 3C8.433 3 10 4.567 10 6.5ZM9.30884 10.0159C8.53901 10.6318 7.56251 11 6.5 11C4.01472 11 2 8.98528 2 6.5C2 4.01472 4.01472 2 6.5 2C8.98528 2 11 4.01472 11 6.5C11 7.56251 10.6318 8.53901 10.0159 9.30884L12.8536 12.1464C13.0488 12.3417 13.0488 12.6583 12.8536 12.8536C12.6583 13.0488 12.3417 13.0488 12.1464 12.8536L9.30884 10.0159Z"
+                fill="currentColor"
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+          </Button>
+        </div>
+      </form>
+    </Form>
   );
 }
