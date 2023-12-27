4e679f2542937b46d69ad621c096d9c38c81089e
WavyBoyJodii <najeecasey@gmail.com>
2023-12-27 06:13:31 -0500

    make friend display conditionally display add message and send request buttons

--- src/components/FriendDisplay.tsx
+++ src/components/FriendDisplay.tsx
@@ -1,20 +1,43 @@
 import { User } from "@/lib/types";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import RequestFriendButton from "./RequestFriendButton";
 import AddFriendButton from "./AddFriendButton";
+import MessageFriendButton from "./MessageFriendButton";
+import RemoveFriendButton from "./RemoveFriendButton";
 
 interface FriendDisplayProps {
   friend: User;
+  request?: boolean;
+  add?: boolean;
+  list?: boolean;
 }
 
-export default function FriendDisplay({ friend }: FriendDisplayProps) {
+export default function FriendDisplay({
+  friend,
+  request,
+  add,
+  list,
+}: FriendDisplayProps) {
   return (
-    <div className=" w-20 h-12 flex gap-4 p-3">
+    <div className=" h-12 flex items-center gap-4 p-3">
       <Avatar className=" h-10 w-10">
         <AvatarImage src={friend.profile_photo} />
         <AvatarFallback>{friend.username.substring(0, 1)}</AvatarFallback>
       </Avatar>
       <h3 className=" text-base font-semibold">{friend.username}</h3>
-      <AddFriendButton username={friend.username} />
+      {friend && request ? (
+        <RequestFriendButton friend={friend} />
+      ) : add ? (
+        <div className=" flex gap-3 items-center">
+          <AddFriendButton friend={friend} />
+          <RemoveFriendButton friend={friend} />
+        </div>
+      ) : (
+        <div className=" flex gap-3 items-center">
+          <MessageFriendButton friend={friend} />
+          <RemoveFriendButton friend={friend} />
+        </div>
+      )}
     </div>
   );
 }
