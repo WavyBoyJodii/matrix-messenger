85731c8a6d746ddcd811f875048423612fde20c4
WavyBoyJodii <najeecasey@gmail.com>
2024-01-04 12:20:58 -0500

    modify newchatbtn to reroute app to ai chat route

--- src/components/NewChatButton.tsx
+++ src/components/NewChatButton.tsx
@@ -1,8 +1,19 @@
+"use client";
+
 import { Button } from "./ui/button";
+import { useRouter } from "next/navigation";
 
 export default function NewChatButton() {
+  const router = useRouter();
+
+  const goToAiChat = () => {
+    router.push("/ai");
+  };
   return (
-    <Button className=" w-1/2 flex place-self-center rounded-lg">
+    <Button
+      className=" w-1/2 flex place-self-center rounded-lg"
+      onClick={goToAiChat}
+    >
       <p className=" text-xs  md:text-sm">
         {" "}
         <span className=" hidden sm:inline-flex">New</span> Chat
