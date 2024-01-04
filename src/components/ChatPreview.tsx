c08e46e2a928a65d39a2780b5b78c6abe0f418f9
WavyBoyJodii <najeecasey@gmail.com>
2024-01-04 07:35:33 -0500

    fix style of chatPreview to make display uniform

--- src/components/ChatPreview.tsx
+++ src/components/ChatPreview.tsx
@@ -25,7 +25,7 @@ export default function ChatPreview({
 
   return (
     <Link href={`/chat/${chat.chatId}`}>
-      <div className=" w-36 h-12 flex gap-4 items-center">
+      <div className=" w-full h-12 flex gap-4 items-center">
         <Avatar className=" h-10 w-10">
           <AvatarImage
             src={
@@ -40,8 +40,8 @@ export default function ChatPreview({
               : chat.user1.username.substring(0, 1)}
           </AvatarFallback>
         </Avatar>
-        <div className="flex flex-col gap-1 hidden sm:block">
-          <h3 className=" text-base font-semibold">
+        <div className="flex flex-col gap-1 hidden sm:flex">
+          <h3 className=" text-base font-semibold w-20">
             {myId === chat.user_id1 ? chat.user2.username : chat.user1.username}
           </h3>
           <p className=" text-xs text-gray-700 truncate max-w-[90px]">
