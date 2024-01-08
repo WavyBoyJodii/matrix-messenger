05c80a2093012ffecae7e7f3e61fd44979f97e10
WavyBoyJodii <najeecasey@gmail.com>
2024-01-08 10:43:24 -0500

    change path to slug within matcher

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,10 +5,10 @@ import getAuth from "./lib/getAuth";
 export const config = {
   matcher: [
     "/add-friend",
-    "/chat/:path*",
+    "/chat/:slug*",
     "/friend-list",
     "/requests",
-    "/ai/:path*",
+    "/ai/:slug*",
   ],
 };
 
