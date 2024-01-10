223de33d8bc6a0c0924965b0a72587ec02a207bb
WavyBoyJodii <najeecasey@gmail.com>
2024-01-10 03:43:32 -0500

    logging clear auth cookie function

--- src/lib/clearAuthCookie.ts
+++ src/lib/clearAuthCookie.ts
@@ -3,5 +3,7 @@
 import { cookies } from "next/headers";
 
 export default async function clearAuthCookie() {
+  console.log(`logging clear auth cookie.....`);
   cookies().set("auth", "");
+  console.log(`auth cookie should be cleared...`);
 }
